1e45582e5ead6e85a48459e6b4d7d55d3089b1af
socrateskld <77156381+socrateskld@users.noreply.github.com>
2021-11-07 15:17:08 +0000

    useState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+  const ClickHandler = () => {
+    setTitle("Updated!");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={ClickHandler}>Click ME</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
